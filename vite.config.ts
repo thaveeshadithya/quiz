985f6618888b6e54975648914e631fa769429fb5
thaveesh <tapalliyaguru@gmail.com>
2025-09-11 11:48:55 +0530

    2.3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // if you use React; remove if you don't
 
 export default defineConfig({
-  base: '/quiz/',           // <- CHANGE this to your repo name (trailing slash)
+  base: '/',           // <- CHANGE this to your repo name (trailing slash)
   plugins: [react()],
 })
